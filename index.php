3dffe57ecb13a04711556ec96a7fb69900fc2090
jfarmer32 <43788396+jfarmer32@users.noreply.github.com>
2019-02-25 10:23:50 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/* Group: require_once(teamname.php);
+*  Last edited: 2/24/2019 (V 1.0)
+*  Last edited by: Justin Farmer on 2/24/2019
+*  Purpose: This page will serve as our landing page.
+*/
+?>
+<html>
+  <head>
+    <title>Grid-links</title>
+  </head>
+  <body>
+  
+    <h1>This is a basic landing page.</h1>
+  
+  </body>
+</html>
